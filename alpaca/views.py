f8a6008da9cfd5b16b1a5bfd9fb758e074986047
Hyun Joong Kim <henrynicholas09@gmail.com>
2018-01-06 13:52:04 +0900

    Update views.py

--- alpaca/views.py
+++ alpaca/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json, datetime, csv
+import json, datetime, csv, ast
 
 def keyboard(request):
     pass
