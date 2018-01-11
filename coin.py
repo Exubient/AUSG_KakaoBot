2c8fcca6a200ae26fb57110a4158087b84949471
Hyun Joong Kim <henrynicholas09@gmail.com>
2018-01-11 17:34:49 +0900

    Update coin.py

--- coin.py
+++ coin.py
@@ -7,7 +7,8 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def fetch_cryptocompare():
-    pass
+	#url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=%s&tsyms=%s&e=%s' % (','.join(coins), currency, market)
+	pass
 
 def scheduler():
 	pass
